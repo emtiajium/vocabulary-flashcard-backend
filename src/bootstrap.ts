2f58f24bfcad3b1a783bd0e192e4da292b23154d
emtiajium <271emtiaj@gmail.com>
2021-05-30 13:45:57 +0600

    made it a class

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -6,20 +6,29 @@ import * as cookieParser from 'cookie-parser';
 import AppModule from '@/AppModule';
 import ServiceConfig from '@/common/configs/ServiceConfig';
 
-// TODO transform it to a class
-async function bootstrap(module: AppModule): Promise<INestApplication> {
-    const app: INestApplication = await NestFactory.create(module);
-    app.enableShutdownHooks();
-    app.useGlobalPipes(new ValidationPipe());
-    const serviceConfig = new ServiceConfig();
-    const { payloadLimitSize, serviceApiPrefix, port } = serviceConfig;
-    app.use(bodyParser.json({ limit: payloadLimitSize }));
-    app.use(bodyParser.urlencoded({ limit: payloadLimitSize, parameterLimit: 10_000_000, extended: true }));
-    app.use(cookieParser());
-    app.setGlobalPrefix(serviceApiPrefix);
-    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-    await app.listen(port);
-    return app;
+class Bootstrap {
+    private serviceConfig: ServiceConfig;
+
+    constructor(private readonly appModule: AppModule) {
+        this.serviceConfig = new ServiceConfig();
+    }
+
+    async start(): Promise<INestApplication> {
+        const app: INestApplication = await NestFactory.create(this.appModule);
+        app.enableShutdownHooks();
+        app.useGlobalPipes(new ValidationPipe());
+        const serviceConfig = new ServiceConfig();
+        const { payloadLimitSize, serviceApiPrefix, port } = serviceConfig;
+        app.use(bodyParser.json({ limit: payloadLimitSize }));
+        app.use(bodyParser.urlencoded({ limit: payloadLimitSize, parameterLimit: 10_000_000, extended: true }));
+        app.use(cookieParser());
+        app.setGlobalPrefix(serviceApiPrefix);
+        app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+        await app.listen(port);
+        return app;
+    }
 }
 
-export default bootstrap;
+export default async function bootstrap(module: AppModule): Promise<INestApplication> {
+    return new Bootstrap(module).start();
+}
