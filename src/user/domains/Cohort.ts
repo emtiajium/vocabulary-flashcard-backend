e077b4cdddbc122a3a9140561071f3eda9dd4b93
emtiajium <271emtiaj@gmail.com>
2021-06-05 20:05:26 +0600

    added name of the cohort

--- src/user/domains/Cohort.ts
+++ src/user/domains/Cohort.ts
@@ -2,10 +2,18 @@ import BaseEntity from '@/user/domains/BaseEntity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 import User from '@/user/domains/User';
 import { Type } from 'class-transformer';
-import { IsArray } from 'class-validator';
+import { IsArray, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export const cohortNameSize = 36;
 
 @Entity('Cohort')
 export default class Cohort extends BaseEntity {
+    @MaxLength(cohortNameSize)
+    @IsNotEmpty()
+    @IsString()
+    @Column({ type: 'varchar', unique: true })
+    name: string;
+
     @IsArray()
     @Type(() => User)
     @ManyToOne(() => User, (user) => user.cohort)
