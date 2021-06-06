d9f0abfdf37f12e7e44561ba05d1e0c9297f8406
emtiajium <271emtiaj@gmail.com>
2021-06-06 19:35:43 +0600

    fixed bug of checking invalid users

--- src/user/services/CohortService.ts
+++ src/user/services/CohortService.ts
@@ -15,15 +15,15 @@ export default class CohortService {
     async addUsersToCohort(name: string, userIds: string[]): Promise<void> {
         const cohort: Cohort = await this.cohortRepository.findOneOrFail({ name });
         const currentUserIdsBelongToCohort: string[] = cohort.userIds;
-        await this.validateUsers(userIds, currentUserIdsBelongToCohort);
+        await this.validateUsers(userIds);
         await this.cohortRepository.updateUsersToCohort(cohort.id, _.union(currentUserIdsBelongToCohort, userIds));
         await Promise.all(userIds.map((userId) => this.userRepository.updateCohort(userId, cohort.id)));
     }
 
-    private async validateUsers(userIds: string[], currentUserIdsBelongToCohort: string[]): Promise<void> {
+    private async validateUsers(userIds: string[]): Promise<void> {
         const users = await this.userRepository.getUsers(userIds);
         if (users.length !== userIds.length) {
-            const nonExistingUsers = _.difference(userIds, currentUserIdsBelongToCohort);
+            const nonExistingUsers = _.difference(userIds, _.map(users, 'id'));
             throw new NotFoundException(`There are no such users having IDs ${nonExistingUsers.join(', ')}`);
         }
     }
