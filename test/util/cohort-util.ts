edd7c0eb8a60afe753910ee6614af9fa0d01a9e6
emtiajium <271emtiaj@gmail.com>
2021-06-06 23:28:05 +0600

    added create cohort method

--- test/util/cohort-util.ts
+++ test/util/cohort-util.ts
@@ -8,3 +8,7 @@ export default async function getCohortByName(name: string): Promise<Cohort> {
 export async function removeCohortByName(name: string): Promise<void> {
     await getRepository(Cohort).delete({ name });
 }
+
+export async function createCohort(cohort: Cohort): Promise<Cohort> {
+    return getRepository(Cohort).save(cohort);
+}
