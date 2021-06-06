4f1d342dcbe6a7dc0ceffa7c235bf2777b0a3cd7
emtiajium <271emtiaj@gmail.com>
2021-06-06 15:19:56 +0600

    made users/userIds as optional

--- src/user/domains/Cohort.ts
+++ src/user/domains/Cohort.ts
@@ -2,7 +2,7 @@ import BaseEntity from '@/user/domains/BaseEntity';
 import { Column, Entity, OneToMany } from 'typeorm';
 import User from '@/user/domains/User';
 import { Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { IsArray, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 export const cohortNameSize = 36;
 
@@ -14,12 +14,15 @@ export default class Cohort extends BaseEntity {
     @Column({ type: 'varchar', unique: true })
     name: string;
 
+    @IsOptional()
+    @IsArray()
     @Column({ type: 'uuid', array: true })
     // why do we need it?
     userIds?: string[];
 
+    @IsOptional()
     @IsArray()
     @Type(() => User)
     @OneToMany(() => User, (user) => user.cohort, { eager: false, cascade: false })
-    users: User[];
+    users?: User[];
 }
