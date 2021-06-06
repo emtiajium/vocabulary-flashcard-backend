3a32c5dd4bc2ae8f549ac4af2feabad7a60b175b
emtiajium <271emtiaj@gmail.com>
2021-06-06 15:48:08 +0600

    added several util methods

--- test/util/user-util.ts
+++ test/util/user-util.ts
@@ -1,12 +1,20 @@
 import User from '@/user/domains/User';
-import { getRepository } from 'typeorm';
+import { getRepository, In } from 'typeorm';
 
 export default async function getUserByUsername(username: string): Promise<User> {
     return getRepository(User).findOne({ username });
 }
 
+export async function getUsersByUsernames(usernames: string[]): Promise<User[]> {
+    return getRepository(User).find({ username: In(usernames) });
+}
+
 export async function removeUserByUsername(username: string): Promise<void> {
     await getRepository(User).delete({ username });
+}
+
+export async function removeUsersByCohortName(cohortName: string): Promise<void> {
+    await getRepository(User).delete({ cohort: { name: cohortName } });
 }
 
 export async function createUser(user: User): Promise<User> {
