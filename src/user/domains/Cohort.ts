3c0efd9ace5c6cf5e5dd6c650f189d80bfe60865
emtiajium <271emtiaj@gmail.com>
2021-06-06 22:24:11 +0600

    added validation for UUID

--- src/user/domains/Cohort.ts
+++ src/user/domains/Cohort.ts
@@ -3,6 +3,7 @@ import { Column, Entity, OneToMany } from 'typeorm';
 import User from '@/user/domains/User';
 import { Type } from 'class-transformer';
 import { IsArray, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import IsArrayContainsOnlyUuid from '@/common/validators/IsArrayContainsOnlyUuid';
 
 export const cohortNameSize = 36;
 
@@ -14,14 +15,14 @@ export default class Cohort extends BaseEntity {
     @Column({ type: 'varchar', unique: true })
     name: string;
 
+    @IsArrayContainsOnlyUuid()
     @IsOptional()
-    @IsArray()
     @Column({ type: 'uuid', array: true })
     // why do we need it?
     userIds?: string[];
 
-    @IsOptional()
     @IsArray()
+    @IsOptional()
     @Type(() => User)
     @OneToMany(() => User, (user) => user.cohort, { eager: false, cascade: false })
     users?: User[];
