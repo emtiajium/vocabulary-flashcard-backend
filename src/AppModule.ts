24de36cf716572acdd7dfc9658b26bf3f3b8c813
emtiajium <271emtiaj@gmail.com>
2021-06-13 17:17:09 +0600

    added vocabulary module

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -3,8 +3,15 @@ import UserModule from '@/user/UserModule';
 import { ConfigModule } from '@nestjs/config';
 import DatabaseModule from '@/DatabaseModule';
 import AutorunScriptsModule from '@/AutorunScriptsModule';
+import VocabularyModule from '@/vocabulary/VocabularyModule';
 
 @Module({
-    imports: [ConfigModule.forRoot({ envFilePath: '.env' }), DatabaseModule, UserModule, AutorunScriptsModule],
+    imports: [
+        ConfigModule.forRoot({ envFilePath: '.env' }),
+        DatabaseModule,
+        UserModule,
+        AutorunScriptsModule,
+        VocabularyModule,
+    ],
 })
 export default class AppModule {}
