d51a7b317e1b275d0fd5276952ebee962e243987
emtiajium <271emtiaj@gmail.com>
2021-06-13 12:49:05 +0600

    script to add default cohort

---
+++ src/auto-run-scripts/1623557103708-create-default-cohort.ts
@@ -0,0 +1,30 @@
+import CohortRepository from '@/user/repositories/CohortRepository';
+import Cohort, { defaultName as defaultCohortName } from '@/user/domains/Cohort';
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export default class CreateDefaultCohort {
+    constructor(private readonly cohortRepository: CohortRepository, private readonly logger: Logger) {
+        this.logger = new Logger(CreateDefaultCohort.name);
+    }
+
+    async runScript(): Promise<void> {
+        await this.createDefaultCohort();
+    }
+
+    async createDefaultCohort(): Promise<void> {
+        try {
+            const name = defaultCohortName;
+            await this.cohortRepository
+                .createQueryBuilder()
+                .insert()
+                .into(Cohort)
+                .values({ name: () => `'${name}'::VARCHAR`, userIds: () => `ARRAY []::UUID[]` })
+                .onConflict(`("name") DO NOTHING`)
+                .execute();
+            this.logger.log(`Default cohort with name "${defaultCohortName}" has been created`);
+        } catch {
+            this.logger.error(`Something went wrong while creating the default cohort`);
+        }
+    }
+}
