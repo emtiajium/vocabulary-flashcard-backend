719bdd4bc9ac37d5f33335aefbf699dc8edc2d47
emtiajium <271emtiaj@gmail.com>
2021-06-13 13:32:10 +0600

    dynamic file name getter

--- src/auto-run-scripts/index.ts
+++ src/auto-run-scripts/index.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import { INestApplication, Injectable, Logger } from '@nestjs/common';
 import CreateDefaultCohort from '@/auto-run-scripts/1623557103708-create-default-cohort';
 import { ClassType } from '@/common/types/ClassType';
@@ -12,14 +13,33 @@ export default class AutoRunScripts {
     }
 
     async runScripts(): Promise<void> {
-        const files = this.getFiles();
+        const files = await this.getScripts();
+        // parallel execution is okay for now
+        // we may need to execute the scripts sequentially in future
         await Promise.all(
-            files.map((file) => {
-                return this.app.get(file.name).runScript();
+            files.map((script) => {
+                return this.app.get(script.name).runScript();
             }),
         );
         this.logger.log(`All scripts have been executed`);
     }
 
-    private getFiles = (): ClassType<CreateDefaultCohort>[] => [CreateDefaultCohort];
+    private async getScripts(): Promise<ClassType<CreateDefaultCohort>[]> {
+        const files = await this.getScriptNames();
+        return files.map((file) => {
+            const script = require(`./${file}`);
+            return script.default;
+        });
+    }
+
+    async getScriptNames(): Promise<string[]> {
+        return new Promise((resolve, reject) => {
+            fs.readdir(__dirname, (error, files) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(files.filter((file) => file.endsWith('.js') && file !== 'index.js'));
+            });
+        });
+    }
 }
