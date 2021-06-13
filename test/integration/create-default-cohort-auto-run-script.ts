b96a60ca37ad434a6198e2b1094792bbaa50283d
emtiajium <271emtiaj@gmail.com>
2021-06-13 14:29:10 +0600

    added test case for default cohort creation autorun script

---
+++ test/integration/create-default-cohort-auto-run-script.ts
@@ -0,0 +1,33 @@
+import bootstrap from '@/bootstrap';
+import AppModule from '@/AppModule';
+import { INestApplication } from '@nestjs/common';
+import AutoRunScripts from '@/auto-run-scripts';
+import getCohortByName from '@test/util/cohort-util';
+import { defaultName } from '@/user/domains/Cohort';
+
+describe('Create Default Cohort Autorun Script', () => {
+    let app: INestApplication;
+
+    let mockGetScriptNames;
+
+    beforeAll(async () => {
+        app = await bootstrap(AppModule);
+    });
+
+    afterAll(async () => {
+        mockGetScriptNames.restoreMock();
+        await app.close();
+    });
+
+    it('SHOULD execute the autorun scripts', async () => {
+        mockGetScriptNames = jest
+            .spyOn(AutoRunScripts.prototype, 'getScriptNames')
+            .mockImplementation(() => Promise.resolve(['1623557103708-create-default-cohort.ts']));
+
+        await new AutoRunScripts(app).runScripts();
+
+        const defaultCohort = await getCohortByName(defaultName);
+        expect(defaultCohort.name).toBe(defaultName);
+        expect(defaultCohort.id).toBeDefined();
+    });
+});
