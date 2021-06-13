0477f2ac74bdcd8f7a42f9d47c826a9682802e42
emtiajium <271emtiaj@gmail.com>
2021-06-13 14:15:58 +0600

    added test case for autorun scripts

---
+++ test/integration/auto-run-scripts.ts
@@ -0,0 +1,27 @@
+import bootstrap from '@/bootstrap';
+import AppModule from '@/AppModule';
+import { INestApplication } from '@nestjs/common';
+import AutoRunScripts from '@/auto-run-scripts';
+
+describe('Autorun Scripts', () => {
+    let app: INestApplication;
+
+    let mockRunScripts;
+
+    beforeAll(async () => {
+        app = await bootstrap(AppModule);
+    });
+
+    afterAll(async () => {
+        mockRunScripts.restoreMock();
+        await app.close();
+    });
+
+    it('SHOULD execute the autorun scripts', async () => {
+        mockRunScripts = jest.spyOn(AutoRunScripts.prototype, 'runScripts').mockImplementation(() => Promise.resolve());
+
+        await new AutoRunScripts(app).runScripts().finally();
+
+        expect(mockRunScripts).toHaveBeenCalled();
+    });
+});
