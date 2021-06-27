1d954bf292918673ca7d9a119d4e9c7710e845c4
emtiajium <271emtiaj@gmail.com>
2021-06-27 16:06:55 +0600

    removed TypeOrmModule

--- src/user/UserModule.ts
+++ src/user/UserModule.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import UserController from '@/user/controllers/UserController';
 import UserService from '@/user/services/UserService';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import CohortService from '@/user/services/CohortService';
 import CohortController from '@/user/controllers/CohortController';
 
 @Module({
     controllers: [UserController, CohortController],
     providers: [UserService, CohortService],
-    exports: [TypeOrmModule],
 })
 export default class UserModule {}
--- src/vocabulary/VocabularyModule.ts
+++ src/vocabulary/VocabularyModule.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import VocabularyService from '@/vocabulary/services/VocabularyService';
 import VocabularyController from '@/vocabulary/controllers/VocabularyController';
 
 @Module({
-    exports: [TypeOrmModule],
     providers: [VocabularyService],
     controllers: [VocabularyController],
 })
