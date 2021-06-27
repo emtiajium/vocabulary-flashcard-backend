b627c812fb99c4c9d4f2e3b4f629a607cbd6661c
emtiajium <271emtiaj@gmail.com>
2021-06-27 16:42:43 +0600

    added comment

--- src/DatabaseModule.ts
+++ src/DatabaseModule.ts
@@ -20,6 +20,11 @@ import DefinitionRepository from '@/vocabulary/repositories/DefinitionRepository
                 ]),
             ],
             useFactory: (): TypeOrmModuleOptions => {
+                // do we need it?
+                // https://typeorm.io/#/using-ormconfig/which-configuration-file-is-used-by-typeorm
+                // Which configuration file is used by Typeorm?
+                // From the environment variables. Typeorm will attempt to load the .env file using dotEnv if it exists.
+                // ...
                 const databaseConfig = new DatabaseConfig();
                 const {
                     host,
