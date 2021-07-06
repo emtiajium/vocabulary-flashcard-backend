3d57156c7b33ff7abf039ec163ab02c379d5a5b6
emtiajium <271emtiaj@gmail.com>
2021-07-06 17:42:13 +0600

    fixed casting issue

--- scripts/index.ts
+++ scripts/index.ts
@@ -3,10 +3,10 @@ import InsertVocabularies from './InsertVocabularies';
 
 export default async function executeManualScript(): Promise<void> {
     const configService = new ConfigService();
-    if (configService.get<boolean>('EXECUTE_MANUAL_SCRIPT')) {
+    if (configService.get<string>('EXECUTE_MANUAL_SCRIPT') === 'true') {
         if (
             configService.get<string>('MANUAL_SCRIPT_NAME') === 'InsertVocabularies' &&
-            !configService.get<boolean>('HAS_SCRIPT_INSERT_VOCABULARIES_BEEN_EXECUTED')
+            configService.get<string>('HAS_SCRIPT_INSERT_VOCABULARIES_BEEN_EXECUTED') === 'false'
         ) {
             await new InsertVocabularies().execute();
         }
