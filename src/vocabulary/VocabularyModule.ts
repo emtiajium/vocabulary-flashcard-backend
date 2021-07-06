efcec47b641e3e986456c82ec56535b9a8b4b4a8
emtiajium <271emtiaj@gmail.com>
2021-07-06 21:43:20 +0600

    added provider, repository

--- src/vocabulary/VocabularyModule.ts
+++ src/vocabulary/VocabularyModule.ts
@@ -4,10 +4,14 @@ import VocabularyController from '@/vocabulary/controllers/VocabularyController'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import VocabularyRepository from '@/vocabulary/repositories/VocabularyRepository';
 import DefinitionRepository from '@/vocabulary/repositories/DefinitionRepository';
+import UserService from '@/user/services/UserService';
+import UserRepository from '@/user/repositories/UserRepository';
+import CohortService from '@/user/services/CohortService';
+import CohortRepository from '@/user/repositories/CohortRepository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([VocabularyRepository, DefinitionRepository])],
-    providers: [VocabularyService],
+    imports: [TypeOrmModule.forFeature([UserRepository, CohortRepository, VocabularyRepository, DefinitionRepository])],
+    providers: [UserService, CohortService, VocabularyService],
     controllers: [VocabularyController],
 })
 export default class VocabularyModule {}
