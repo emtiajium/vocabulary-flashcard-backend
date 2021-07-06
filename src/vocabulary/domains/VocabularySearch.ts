51aed616b5731816878af7eef957d596b122963e
emtiajium <271emtiaj@gmail.com>
2021-07-06 21:43:52 +0600

    removed cohortId

--- src/vocabulary/domains/VocabularySearch.ts
+++ src/vocabulary/domains/VocabularySearch.ts
@@ -1,14 +1,9 @@
-import { IsDefined, IsNotEmpty, IsOptional, IsUUID, ValidateNested } from 'class-validator';
+import { IsNotEmpty, IsOptional, ValidateNested } from 'class-validator';
 import Pagination from '@/common/domains/Pagination';
 import { Type } from 'class-transformer';
 import Sort from '@/common/domains/Sort';
 
 export default class VocabularySearch {
-    @IsUUID()
-    @IsNotEmpty()
-    @IsDefined()
-    cohortId: string;
-
     @IsNotEmpty()
     @ValidateNested({ each: true })
     @Type(() => Pagination)
