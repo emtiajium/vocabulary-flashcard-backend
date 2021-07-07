23147aace35d61e0cac10e905743505f41150d9e
emtiajium <271emtiaj@gmail.com>
2021-07-07 18:02:59 +0600

    fixed linter issue

--- scripts/index.ts
+++ scripts/index.ts
@@ -4,11 +4,9 @@ import InsertVocabularies from './InsertVocabularies';
 export default async function executeManualScript(): Promise<void> {
     const configService = new ConfigService();
     if (configService.get<string>('EXECUTE_MANUAL_SCRIPT') === 'true') {
-        if (
-            configService.get<string>('MANUAL_SCRIPT_NAME') === 'InsertVocabularies' &&
-            configService.get<string>('HAS_SCRIPT_INSERT_VOCABULARIES_BEEN_EXECUTED') === 'false'
-        ) {
-            await new InsertVocabularies().execute();
-        }
+        await (configService.get<string>('MANUAL_SCRIPT_NAME') === 'InsertVocabularies' &&
+        configService.get<string>('HAS_SCRIPT_INSERT_VOCABULARIES_BEEN_EXECUTED') === 'false'
+            ? new InsertVocabularies().execute()
+            : Promise.resolve());
     }
 }
