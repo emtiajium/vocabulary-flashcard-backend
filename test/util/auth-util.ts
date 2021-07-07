f08d244faa3183ff121f3d7dcf49c7ec4c95deca
emtiajium <271emtiaj@gmail.com>
2021-07-07 14:04:15 +0600

    added comment

--- test/util/auth-util.ts
+++ test/util/auth-util.ts
@@ -3,9 +3,12 @@ import { sign, Algorithm } from 'jsonwebtoken';
 import * as fs from 'fs';
 import { classToPlain } from 'class-transformer';
 
+// to encode a jwt: https://jwt.io/
+// to generate a private key: https://dinochiesa.github.io/jwt/
+
 export default function generateJwToken(user: User): string {
     const plainUser = classToPlain(user);
     const algorithm: Algorithm = 'RS256';
     const privateKey = fs.readFileSync(`${process.cwd()}/test/private.key`);
-    return sign(plainUser, privateKey, { algorithm, expiresIn: '1h' });
+    return sign(plainUser, privateKey, { algorithm, expiresIn: '5m' });
 }
