6dcae5a561ca46b2daa1c185fa69551bacb2c4b7
emtiajium <271emtiaj@gmail.com>
2021-07-07 12:27:46 +0600

    decoded jwt

--- src/common/guards/AuthGuard.ts
+++ src/common/guards/AuthGuard.ts
@@ -1,10 +1,8 @@
 import UserService from '@/user/services/UserService';
 import User from '@/user/domains/User';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Request } from 'express';
-
-// fake auth guard
-// should be validated using JWT token
+import { decode, JwtPayload } from 'jsonwebtoken';
 
 @Injectable()
 export default class AuthGuard implements CanActivate {
@@ -14,15 +12,45 @@ export default class AuthGuard implements CanActivate {
         return request.headers;
     };
 
-    private getUserId(request: Request): string {
-        return this.getHeaders(request)['X-User-Id'.toLowerCase()] as string;
+    private getAuthorization(request: Request): string {
+        return this.getHeaders(request)['Authorization'.toLowerCase()] as string;
+    }
+
+    private getJwToken(request: Request): string {
+        const authorizationHeader = this.getAuthorization(request);
+        if (!authorizationHeader) {
+            throw new ForbiddenException();
+        }
+        const expectedLength = 2;
+        const authorizationParts = authorizationHeader.split('Bearer ');
+        if (!authorizationParts || authorizationParts?.length !== expectedLength) {
+            throw new ForbiddenException();
+        }
+        return authorizationParts[1];
+    }
+
+    private decodeJwToken = (token: string): JwtPayload => {
+        let jwtPayload: JwtPayload;
+        try {
+            jwtPayload = decode(token, { json: true });
+            if (!jwtPayload) {
+                throw new ForbiddenException();
+            }
+        } catch {
+            throw new ForbiddenException();
+        }
+        return jwtPayload;
+    };
+
+    private getUsernameFromToken(request: Request): string {
+        return this.decodeJwToken(this.getJwToken(request)).email;
     }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
         let isValidUser: boolean;
         try {
-            const user: User = await this.userService.getUserById(this.getUserId(request));
+            const user: User = await this.userService.getUserByUsername(await this.getUsernameFromToken(request));
             request.user = user;
             isValidUser = !!user;
         } catch {
