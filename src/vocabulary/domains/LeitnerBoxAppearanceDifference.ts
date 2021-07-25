0f4de2021b9b4967573d6aea8b084e8fe2fad9b1
emtiajium <271emtiaj@gmail.com>
2021-07-25 21:54:08 +0600

    box appearance date calculation

---
+++ src/vocabulary/domains/LeitnerBoxAppearanceDifference.ts
@@ -0,0 +1,9 @@
+enum LeitnerBoxAppearanceDifference {
+    BOX_1 = 0,
+    BOX_2 = 1,
+    BOX_3 = 2,
+    BOX_4 = 3,
+    BOX_5 = 0,
+}
+
+export default LeitnerBoxAppearanceDifference;
--- src/vocabulary/domains/LeitnerSystems.ts
+++ src/vocabulary/domains/LeitnerSystems.ts
@@ -3,6 +3,8 @@ import BaseEntity from '@/common/domains/BaseEntity';
 import LeitnerBoxType from '@/vocabulary/domains/LeitnerBoxType';
 import { IsDate, IsDefined, IsEnum, IsNotEmpty, IsUUID } from 'class-validator';
 import { Type } from 'class-transformer';
+import MomentUnit, { makeItNewer } from '@/common/utils/moment-util';
+import LeitnerBoxAppearanceDifference from '@/vocabulary/domains/LeitnerBoxAppearanceDifference';
 
 // Big NO to foreign key concept
 
@@ -28,4 +30,35 @@ export default class LeitnerSystems extends BaseEntity {
     @Type(() => Date)
     @IsDefined()
     boxAppearanceDate: Date;
+
+    static calculateNextBoxAppearanceDate(box: LeitnerBoxType): Date {
+        let boxAppearanceDate: Date;
+        switch (box) {
+            case LeitnerBoxType.BOX_1: {
+                boxAppearanceDate = new Date();
+                break;
+            }
+            case LeitnerBoxType.BOX_2: {
+                boxAppearanceDate = makeItNewer(new Date(), MomentUnit.DAYS, LeitnerBoxAppearanceDifference.BOX_2);
+                break;
+            }
+            case LeitnerBoxType.BOX_3: {
+                boxAppearanceDate = makeItNewer(new Date(), MomentUnit.DAYS, LeitnerBoxAppearanceDifference.BOX_3);
+                break;
+            }
+            case LeitnerBoxType.BOX_4: {
+                boxAppearanceDate = makeItNewer(new Date(), MomentUnit.DAYS, LeitnerBoxAppearanceDifference.BOX_4);
+                break;
+            }
+            default: {
+                // LeitnerBoxType.BOX_5
+                boxAppearanceDate = new Date();
+            }
+        }
+        return boxAppearanceDate;
+    }
+
+    static calculatePreviousBoxAppearanceDate(): Date {
+        return new Date();
+    }
 }
