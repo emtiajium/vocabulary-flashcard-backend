62b20a6e27d843630760c16a6ff041236fe7a06b
emtiajium <271emtiaj@gmail.com>
2021-07-25 21:33:55 +0600

    added moment util

---
+++ src/common/utils/moment-util.ts
@@ -0,0 +1,56 @@
+enum MomentUnit {
+    DAYS = 'DAYS',
+    HOURS = 'HOURS',
+    MINUTES = 'MINUTES',
+    SECONDS = 'SECONDS',
+}
+
+export default MomentUnit;
+
+export function makeItOlder(now: Date, unit: MomentUnit, period: number): Date {
+    const clonedNow = new Date(now.valueOf());
+    if (unit === MomentUnit.DAYS) {
+        return new Date(clonedNow.setDate(clonedNow.getDate() - period));
+    }
+    if (unit === MomentUnit.HOURS) {
+        return new Date(clonedNow.setHours(clonedNow.getHours() - period));
+    }
+    if (unit === MomentUnit.MINUTES) {
+        return new Date(clonedNow.setMinutes(clonedNow.getMinutes() - period));
+    }
+    return new Date(clonedNow.setSeconds(clonedNow.getSeconds() - period));
+}
+
+export function makeItNewer(now: Date, unit: MomentUnit, period: number): Date {
+    const clonedNow = new Date(now.valueOf());
+    if (unit === MomentUnit.DAYS) {
+        return new Date(clonedNow.setDate(clonedNow.getDate() + period));
+    }
+    if (unit === MomentUnit.HOURS) {
+        return new Date(clonedNow.setHours(clonedNow.getHours() + period));
+    }
+    if (unit === MomentUnit.MINUTES) {
+        return new Date(clonedNow.setMinutes(clonedNow.getMinutes() + period));
+    }
+    return new Date(clonedNow.setSeconds(clonedNow.getSeconds() + period));
+}
+
+const oneMinuteInSeconds = 60;
+
+const oneDayInHours = 24;
+
+const oneSecondInMilliSeconds = 1000;
+
+export function momentDiff(firstMoment: Date, secondMoment: Date, unit: MomentUnit): number {
+    const differenceInSeconds = Math.abs(firstMoment.getTime() - secondMoment.getTime()) / oneSecondInMilliSeconds;
+    if (unit === MomentUnit.DAYS) {
+        return Math.ceil(differenceInSeconds / (oneMinuteInSeconds * oneMinuteInSeconds * oneDayInHours));
+    }
+    if (unit === MomentUnit.HOURS) {
+        return Math.ceil(differenceInSeconds / (oneMinuteInSeconds * oneMinuteInSeconds));
+    }
+    if (unit === MomentUnit.MINUTES) {
+        return Math.ceil(differenceInSeconds / oneMinuteInSeconds);
+    }
+    return Math.ceil(differenceInSeconds);
+}
