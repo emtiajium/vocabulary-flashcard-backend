6931e8c82f130c02fae8441f02d82702f9caefb5
emtiajium <271emtiaj@gmail.com>
2021-07-26 00:14:51 +0600

    added LeitnerSystems table

--- src/vocabulary/VocabularyModule.ts
+++ src/vocabulary/VocabularyModule.ts
@@ -8,9 +8,18 @@ import UserService from '@/user/services/UserService';
 import UserRepository from '@/user/repositories/UserRepository';
 import CohortService from '@/user/services/CohortService';
 import CohortRepository from '@/user/repositories/CohortRepository';
+import LeitnerSystems from '@/vocabulary/domains/LeitnerSystems';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository, CohortRepository, VocabularyRepository, DefinitionRepository])],
+    imports: [
+        TypeOrmModule.forFeature([
+            UserRepository,
+            CohortRepository,
+            VocabularyRepository,
+            DefinitionRepository,
+            LeitnerSystems,
+        ]),
+    ],
     providers: [UserService, CohortService, VocabularyService],
     controllers: [VocabularyController],
 })
