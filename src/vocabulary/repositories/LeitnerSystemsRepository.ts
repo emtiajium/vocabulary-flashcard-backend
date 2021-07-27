ab63d39d652175c4739a2c9996b1cf97c3c5c465
emtiajium <271emtiaj@gmail.com>
2021-07-27 16:17:13 +0600

    getBoxItems()

--- src/vocabulary/repositories/LeitnerSystemsRepository.ts
+++ src/vocabulary/repositories/LeitnerSystemsRepository.ts
@@ -1,5 +1,31 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Raw, Repository } from 'typeorm';
 import LeitnerSystems from '@/vocabulary/domains/LeitnerSystems';
+import LeitnerBoxType from '@/vocabulary/domains/LeitnerBoxType';
+import SearchResult from '@/common/domains/SearchResult';
+import Pagination from '@/common/domains/Pagination';
+import { getToday } from '@/common/utils/moment-util';
 
 @EntityRepository(LeitnerSystems)
-export default class LeitnerSystemsRepository extends Repository<LeitnerSystems> {}
+export default class LeitnerSystemsRepository extends Repository<LeitnerSystems> {
+    async getBoxItems(
+        userId: string,
+        box: LeitnerBoxType,
+        pagination: Pagination,
+    ): Promise<SearchResult<LeitnerSystems>> {
+        const { pageSize, pageNumber } = pagination;
+        const toBeSkipped = pageSize * (pageNumber - 1);
+
+        const [items, total] = await this.findAndCount({
+            where: {
+                userId,
+                currentBox: box,
+                boxAppearanceDate: Raw((alias) => `${alias} <= '${getToday()}'::DATE`),
+            },
+            select: ['id'],
+            skip: toBeSkipped,
+            take: pageSize,
+        });
+
+        return new SearchResult<LeitnerSystems>(items, total);
+    }
+}
