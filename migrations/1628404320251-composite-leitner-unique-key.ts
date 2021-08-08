41690db7edbce5a633dc94560d46b8b629410ed1
emtiajium <271emtiaj@gmail.com>
2021-08-08 13:31:28 +0600

    temp commit: 'Key (userId, vocabularyId)=(0aad83c6-a545-49b0-b561-d11d83577e09, 0338d7b3-bf92-4593-b60c-b1843af05277) is duplicated.'

--- migrations/1628404320251-composite-leitner-unique-key.ts
+++ migrations/1628404320251-composite-leitner-unique-key.ts
@@ -2,12 +2,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export default class CompositeLeitnerUniqueKey1628404320251 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `CREATE UNIQUE INDEX "IDX_26b3aff7cb4ec7b6983d549c5e" ON "LeitnerSystems" ("userId", "vocabularyId")`,
-        );
+        // await queryRunner.query(
+        //     `CREATE UNIQUE INDEX "IDX_26b3aff7cb4ec7b6983d549c5e" ON "LeitnerSystems" ("userId", "vocabularyId")`,
+        // );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "IDX_26b3aff7cb4ec7b6983d549c5e"`);
+        // await queryRunner.query(`DROP INDEX "IDX_26b3aff7cb4ec7b6983d549c5e"`);
     }
 }
