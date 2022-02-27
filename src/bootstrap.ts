f0c8ca3b389e7fee2083a03917c9c871753fc4d0
Emtiaj Hasan <ehasan@testreach.com>
2022-02-27 23:55:15 +0600

    enabled HTTPS

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,21 +5,22 @@ import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import AppModule from '@/AppModule';
 import ServiceConfig from '@/common/configs/ServiceConfig';
+import * as fs from 'fs';
+import { NestApplicationOptions } from '@nestjs/common/interfaces/nest-application-options.interface';
 
 class Bootstrap {
-    private serviceConfig: ServiceConfig;
+    private readonly serviceConfig: ServiceConfig;
 
     constructor(private readonly appModule: AppModule) {
         this.serviceConfig = new ServiceConfig();
     }
 
     async start(): Promise<INestApplication> {
-        const app: INestApplication = await NestFactory.create(this.appModule);
+        const app: INestApplication = await NestFactory.create(this.appModule, this.getAppOptions());
         app.enableCors();
         app.enableShutdownHooks();
         app.useGlobalPipes(new ValidationPipe());
-        const serviceConfig = new ServiceConfig();
-        const { payloadLimitSize, serviceApiPrefix, port } = serviceConfig;
+        const { payloadLimitSize, serviceApiPrefix, port } = this.serviceConfig;
         app.use(bodyParser.json({ limit: payloadLimitSize }));
         app.use(bodyParser.urlencoded({ limit: payloadLimitSize, parameterLimit: 10_000_000, extended: true }));
         app.use(cookieParser());
@@ -27,6 +28,21 @@ class Bootstrap {
         app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
         await app.listen(port);
         return app;
+    }
+
+    private getAppOptions(): NestApplicationOptions {
+        let options: NestApplicationOptions;
+
+        if (this.serviceConfig.environment === 'development') {
+            options = {
+                httpsOptions: {
+                    key: fs.readFileSync('cert/CA/localhost/localhost.decrypted.key'),
+                    cert: fs.readFileSync('cert/CA/localhost/localhost.crt'),
+                },
+            };
+        }
+
+        return options;
     }
 }
 
