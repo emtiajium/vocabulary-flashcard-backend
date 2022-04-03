d8fd0b7365f9303e69c263b2cd880d38d788a5dc
Emtiaj Hasan <271emtiaj@gmail.com>
2022-04-03 23:25:57 +0600

    ping to the @Get(/v1/androids)

--- src/health-check/HealthCheckModule.ts
+++ src/health-check/HealthCheckModule.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import HealthCheckController from '@/health-check/controllers/HealthCheckController';
 import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
     controllers: [HealthCheckController],
-    imports: [TerminusModule],
+    imports: [TerminusModule, HttpModule],
+    providers: [ConfigService, Logger],
 })
 export default class HealthCheckModule {}
