f06331b9cc892552393ec26699af7b19198012ac
Emtiaj Hasan <271emtiaj@gmail.com>
2022-04-07 09:54:48 +0600

    removed validation over the length

--- src/user/domains/User.ts
+++ src/user/domains/User.ts
@@ -1,39 +1,30 @@
 import BaseEntity from '@/common/domains/BaseEntity';
 import { Column, Entity, ManyToOne } from 'typeorm';
-import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl } from 'class-validator';
 import { Expose } from 'class-transformer';
 import Cohort from '@/user/domains/Cohort';
 import { ApiHideProperty } from '@nestjs/swagger';
 
-export const usernameSize = 36;
-export const firstnameSize = 36;
-export const lastnameSize = 36;
-export const profilePictureUrlSize = 100;
-
 @Entity('User')
 export default class User extends BaseEntity {
     @Column({ type: 'varchar', unique: true })
-    @MaxLength(usernameSize)
     @IsEmail()
     @IsString()
     @IsNotEmpty()
     username: string;
 
     @Column({ type: 'varchar' })
-    @MaxLength(firstnameSize)
     @IsString()
     @IsNotEmpty()
     firstname: string;
 
     @Column({ type: 'varchar', nullable: true })
-    @MaxLength(lastnameSize)
     @IsString()
     @IsOptional()
     lastname?: string;
 
     @Column({ type: 'varchar', nullable: true })
     @IsUrl()
-    @MaxLength(profilePictureUrlSize)
     @IsString()
     @IsOptional()
     profilePictureUrl?: string;
