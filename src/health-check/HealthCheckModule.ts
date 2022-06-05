e61dabf8db6e044e4a7a91a994dd68fe21c3ec7d
Emtiaj Hasan <271emtiaj@gmail.com>
2022-06-05 16:03:13 +0600

    removed pinging to certain endpoints

--- src/health-check/HealthCheckModule.ts
+++ src/health-check/HealthCheckModule.ts
@@ -1,12 +1,9 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import HealthCheckController from '@/health-check/controllers/HealthCheckController';
 import { TerminusModule } from '@nestjs/terminus';
-import { HttpModule } from '@nestjs/axios';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
     controllers: [HealthCheckController],
-    imports: [TerminusModule, HttpModule],
-    providers: [ConfigService, Logger],
+    imports: [TerminusModule],
 })
 export default class HealthCheckModule {}
--- src/health-check/controllers/HealthCheckController.ts
+++ src/health-check/controllers/HealthCheckController.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiSecurity } from '@nestjs/swagger';
-import { HealthCheck, HttpHealthIndicator, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import { HealthCheck, TypeOrmHealthIndicator } from '@nestjs/terminus';
 import { HealthIndicatorResult } from '@nestjs/terminus/dist/health-indicator';
 import { InjectConnection } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('/v1/health')
 @ApiSecurity('Authorization')
@@ -13,35 +12,11 @@ export default class HealthCheckController {
         @InjectConnection()
         private connection: Connection,
         private readonly dbHealthIndicator: TypeOrmHealthIndicator,
-        private readonly httpHealthIndicator: HttpHealthIndicator,
-        private readonly configService: ConfigService,
-        private readonly logger: Logger,
     ) {}
 
     @Get()
     @HealthCheck()
-    async check(): Promise<HealthIndicatorResult> {
-        // dummy health-check
-        // https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-instance-addressing.html#concepts-public-addresses
-        // summary: I somehow need to prevent AWS to release IP addresses that are assigned
-        // TODO do it with Cron Job
-        const ipAddresses = this.configService.get('IP_ADDRESSES')
-            ? this.configService.get('IP_ADDRESSES').split(',')
-            : [];
-
-        if (ipAddresses.length > 0) {
-            const apiPrefix = this.configService.get('SERVICE_API_PREFIX');
-
-            await Promise.allSettled(
-                ipAddresses.map((ipAddress, index) => {
-                    return this.httpHealthIndicator
-                        .pingCheck(`ping${index + 1}`, `https://${ipAddress}${apiPrefix}/v1/androids`)
-                        .finally(() => this.logger.log('Ping!', HealthCheckController.name));
-                }),
-            );
-        }
-
-        // actual health-check
+    check(): Promise<HealthIndicatorResult> {
         return this.dbHealthIndicator.pingCheck('database', { connection: this.connection });
     }
 }
