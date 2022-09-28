ca3bacdf32a66811e191d7d57453f64671e6383f
Emtiaj Hasan <271emtiaj@gmail.com>
2022-09-28 16:21:55 +0600

    prevented creating multiple cron-job

--- .platform/hooks/postdeploy/cron-health-check-endpoint.sh
+++ .platform/hooks/postdeploy/cron-health-check-endpoint.sh
@@ -3,7 +3,15 @@
 endpoint=https://api.firecrackervocabulary.com/rest/ielts-service/v1/health
 clientId=Health-Monitoring-Cron-Job
 
-echo "*/1 * * * * root curl -X 'GET' \
-    '$endpoint' \
-    -H 'X-Client-Id: $clientId' \
-    -H 'accept: application/json'" | sudo tee -a /etc/crontab > /dev/null
+isExist=false
+
+while read -r currentLine
+    do
+        if [[ $currentLine == *$clientId* ]]; then
+            isExist=true
+        fi
+done < "/etc/crontab"
+
+if [[ $isExist == false ]]; then
+    echo "*/1 * * * * root curl -X 'GET' '$endpoint' -H 'X-Client-Id: $clientId' -H 'accept: application/json'" | sudo tee -a /etc/crontab > /dev/null
+fi
