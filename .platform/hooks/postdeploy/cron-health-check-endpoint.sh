429298526ba94b26a0bd188e0e4f23f6d16d86ec
Emtiaj Hasan <271emtiaj@gmail.com>
2022-09-28 15:43:48 +0600

    introduced variables

--- .platform/hooks/postdeploy/cron-health-check-endpoint.sh
+++ .platform/hooks/postdeploy/cron-health-check-endpoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+endpoint=https://api.firecrackervocabulary.com/rest/ielts-service/v1/health
+clientId=Health-Monitoring-Cron-Job
+
 echo "*/1 * * * * root curl -X 'GET' \
-    'https://api.firecrackervocabulary.com/rest/ielts-service/v1/health' \
-    -H 'X-Client-Id: Health-Monitoring-Cron-Job' \
+    '$endpoint' \
+    -H 'X-Client-Id: $clientId' \
     -H 'accept: application/json'" | sudo tee -a /etc/crontab > /dev/null
