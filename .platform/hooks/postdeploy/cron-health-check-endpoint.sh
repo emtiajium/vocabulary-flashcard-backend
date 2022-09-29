d6fa97f6b7383bc731a08ed5a26930a8a8a8263b
Emtiaj Hasan <271emtiaj@gmail.com>
2022-09-29 10:30:18 +0600

    changed ownersip and restart the cron daemon only if it is necessary

--- .platform/hooks/postdeploy/cron-health-check-endpoint.sh
+++ .platform/hooks/postdeploy/cron-health-check-endpoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# write permission to the ec2-user
-sudo chown ec2-user /etc/crontab
-
 servicePort=$(/opt/elasticbeanstalk/bin/get-config environment -k PORT)
 serviceApiPrefix=$(/opt/elasticbeanstalk/bin/get-config environment -k SERVICE_API_PREFIX)
 # I don't want to hit the health-check endpoint using the ec2/eb/https
@@ -19,12 +16,15 @@ while read -r currentLine
 done < "/etc/crontab"
 
 if [[ $isExist == false ]]; then
+    # write permission to the ec2-user
+    sudo chown ec2-user /etc/crontab
+
     echo "*/1 * * * * root curl -X 'GET' '$endpoint' -H 'X-Client-Id: $clientId' -H 'accept: application/json'" | sudo tee -a /etc/crontab > /dev/null
+
+    # below command is to resolve the error
+    # crond[5976]: (root) WRONG FILE OWNER (/etc/crontab)
+    sudo chown root /etc/crontab
+
+    # restart the cron daemon
+    sudo service crond restart
 fi
-
-# below command is to resolve the error
-# crond[5976]: (root) WRONG FILE OWNER (/etc/crontab)
-sudo chown root /etc/crontab
-
-# restart the cron daemon
-sudo service crond restart
