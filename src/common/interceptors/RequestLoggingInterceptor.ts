a6d5d65e72b0f14afea4d100da8e92a9551f5bdf
Emtiaj Hasan <271emtiaj@gmail.com>
2022-10-05 07:48:39 +0600

    prevented logging health-check endpoint

--- src/common/interceptors/RequestLoggingInterceptor.ts
+++ src/common/interceptors/RequestLoggingInterceptor.ts
@@ -13,15 +13,15 @@ export default class RequestLoggingInterceptor implements NestInterceptor {
         return request.headers;
     };
 
-    intercept(executionContext: ExecutionContext, next: CallHandler): Observable<CallHandler> {
-        const request = executionContext.switchToHttp().getRequest();
+    private logRequest(request: Request): void {
         const headers = this.getHeaders(request);
+        const { url, method } = request;
 
         this.logger.log(
             safeStringify({
                 host: headers.host,
-                url: request.url,
-                method: request.method,
+                url,
+                method,
                 'x-real-ip': headers['x-real-ip'],
                 'x-forwarded-for': headers['x-forwarded-for'],
                 'user-agent': headers['user-agent'],
@@ -30,8 +30,18 @@ export default class RequestLoggingInterceptor implements NestInterceptor {
                 referer: headers.referer,
                 client: headers['x-client-id'],
             }),
-            `Request headers`,
+            'REST Request Log',
         );
+    }
+
+    intercept(executionContext: ExecutionContext, next: CallHandler): Observable<CallHandler> {
+        const request = executionContext.switchToHttp().getRequest();
+
+        const isHealthCheckEndpoint = request.url.search('/v1/health') !== -1;
+
+        if (!isHealthCheckEndpoint) {
+            this.logRequest(request);
+        }
 
         return next.handle();
     }
