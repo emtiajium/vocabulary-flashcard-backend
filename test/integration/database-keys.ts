d86ab2c2e55657dc040062c72b501f32570849f7
Emtiaj Hasan <271emtiaj@gmail.com>
2022-10-16 23:48:33 +0600

    changed the test expectation | considered conventional act stage

--- test/integration/database-keys.ts
+++ test/integration/database-keys.ts
@@ -5,6 +5,7 @@ import { Connection, getConnection, getManager } from 'typeorm';
 import { INestApplication } from '@nestjs/common';
 import { kickOff } from '@/bootstrap';
 import AppModule from '@/AppModule';
+import DatabaseNamingStrategy from '@/common/persistence/DatabaseNamingStrategy';
 
 describe('Database Keys', () => {
     let app: INestApplication;
@@ -25,27 +26,32 @@ describe('Database Keys', () => {
 
     test(`Primary Keys`, async () => {
         for (const tableName of tableNames) {
+            const queryResult: { constraintName: string }[] = await getManager().query(
+                `
+                    SELECT tc.constraint_name AS "constraintName"
+                    FROM information_schema.key_column_usage kcu
+                             INNER JOIN information_schema.table_constraints tc
+                                        ON kcu.table_name = tc.table_name
+                                            AND kcu.table_name = $1
+                                            AND tc.table_name = $1
+                                            AND kcu.constraint_name = tc.constraint_name
+                                            AND tc.constraint_type = 'PRIMARY KEY';
+                `,
+                [tableName],
+            );
+
             const { primaryColumns } = dbConnection.entityMetadatas.find(
                 (metadata) => metadata.tableName === tableName,
             );
 
-            for (const primaryColumn of primaryColumns) {
-                const [{ primaryKeyName }] = await getManager().query(
-                    `
-                        SELECT tc.constraint_name AS "primaryKeyName"
-                        FROM information_schema.key_column_usage kcu
-                                 INNER JOIN information_schema.table_constraints tc
-                                            ON kcu.table_name = tc.table_name
-                                                AND kcu.table_name = $1
-                                                AND tc.table_name = $1
-                                                AND kcu.constraint_name = tc.constraint_name
-                                                AND tc.constraint_type = 'PRIMARY KEY';
-                    `,
-                    [tableName],
+            queryResult.forEach((currentQueryResult) => {
+                const primaryKeyName = new DatabaseNamingStrategy().primaryKeyName(
+                    tableName,
+                    primaryColumns.map((primaryColumn) => primaryColumn.databaseName),
                 );
 
-                expect(primaryKeyName).toBe(`PK_${tableName}_${primaryColumn.databaseName}`);
-            }
+                expect(primaryKeyName).toBe(currentQueryResult.constraintName);
+            });
         }
     });
 
@@ -54,11 +60,14 @@ describe('Database Keys', () => {
             const { foreignKeys } = dbConnection.entityMetadatas.find((metadata) => metadata.tableName === tableName);
 
             foreignKeys.forEach((foreignKey) => {
-                foreignKey.columnNames.forEach((foreignKeyColumnName, index) => {
-                    expect(foreignKey.name).toBe(
-                        `FK_${tableName}_${foreignKeyColumnName}_${foreignKey.referencedTablePath}_${foreignKey.referencedColumnNames[index]}`,
-                    );
-                });
+                const foreignKeyName = new DatabaseNamingStrategy().foreignKeyName(
+                    tableName,
+                    foreignKey.columnNames,
+                    foreignKey.referencedTablePath,
+                    foreignKey.referencedColumnNames,
+                );
+
+                expect(foreignKeyName).toBe(foreignKey.name);
             });
         }
     });
@@ -68,9 +77,12 @@ describe('Database Keys', () => {
             const { indices } = dbConnection.entityMetadatas.find((metadata) => metadata.tableName === tableName);
 
             indices.forEach((index) => {
-                expect(index.name).toBe(
-                    `IDX_${tableName}_${index.columns.map((column) => column.databaseName).join('_')}`,
+                const indexName = new DatabaseNamingStrategy().indexName(
+                    tableName,
+                    index.columns.map((column) => column.databaseName),
                 );
+
+                expect(indexName).toBe(index.name);
             });
         }
     });
@@ -82,9 +94,12 @@ describe('Database Keys', () => {
             );
 
             uniqueKeys.forEach((uniqueKey) => {
-                expect(uniqueKey.name).toBe(
-                    `UQ_${tableName}_${uniqueKey.columns.map((column) => column.databaseName).join('_')}`,
+                const uniqueKeyName = new DatabaseNamingStrategy().uniqueConstraintName(
+                    tableName,
+                    uniqueKey.columns.map((column) => column.databaseName),
                 );
+
+                expect(uniqueKeyName).toBe(uniqueKey.name);
             });
         }
     });
