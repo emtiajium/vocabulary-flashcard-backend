4916dfabc67abacae82484613e42abe617b6729b
--get-all <271emtiaj@gmail.com>
2023-02-24 16:33:01 +0600

    added script to delete leitner items

--- practice/sqls/remove-all-by-cohort.sql
+++ practice/sqls/remove-all-by-cohort.sql
@@ -1,12 +1,14 @@
 SET SESSION my.cohortId = 'd70880ac-9dfe-4727-a024-53f365796d8a';
 
 DELETE
+FROM "LeitnerSystems"
+WHERE "vocabularyId" IN (SELECT id FROM "Vocabulary" WHERE "cohortId" = current_setting('my.cohortId')::UUID)
+
+DELETE
 FROM "Definition"
-WHERE "vocabularyId" IN (
-    SELECT id
-    FROM "Vocabulary"
-    WHERE "cohortId" = current_setting('my.cohortId')::UUID
-);
+WHERE "vocabularyId" IN (SELECT id
+                         FROM "Vocabulary"
+                         WHERE "cohortId" = current_setting('my.cohortId')::UUID);
 
 DELETE
 FROM "Vocabulary"
