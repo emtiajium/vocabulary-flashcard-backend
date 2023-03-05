884cdfe0e7ff768ee2f18af74b4a7966da82b22c
--get-all <271emtiaj@gmail.com>
2023-03-05 20:18:58 +0600

    suppressed eslint

--- migrations/1678015289009-relate-linker-words.ts
+++ migrations/1678015289009-relate-linker-words.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 import Vocabulary from '@/vocabulary/domains/Vocabulary';
 
 export default class RelateLinkerWords1678015289009 implements MigrationInterface {
-    // eslint-disable-next-line consistent-return
+    // eslint-disable-next-line consistent-return,complexity
     public async up(queryRunner: QueryRunner): Promise<void> {
         const vocabulariesHavingLinkerWords: Vocabulary[] = await queryRunner.query(`
             SELECT id, word, "linkerWords", "cohortId"
