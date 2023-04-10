01fb1e9176066ff278b02175f289dca0cf20191b
--get-all <271emtiaj@gmail.com>
2023-04-10 14:59:19 +0600

    added npm script test:memory-usage

--- test/jest-init.ts
+++ test/jest-init.ts
@@ -3,3 +3,12 @@ import * as dotenv from 'dotenv';
 dotenv.config({
     path: './test/.env.test',
 });
+
+global.console = {
+    ...console,
+    log: jest.fn(),
+    debug: jest.fn(),
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+};
