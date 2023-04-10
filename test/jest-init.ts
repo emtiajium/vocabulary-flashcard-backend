9e062baff17176ecfedd35935f03ba60b8e17d82
--get-all <271emtiaj@gmail.com>
2023-04-10 15:01:50 +0600

    Revert "added npm script test:memory-usage"
    This reverts commit 01fb1e9176066ff278b02175f289dca0cf20191b.

--- test/jest-init.ts
+++ test/jest-init.ts
@@ -3,12 +3,3 @@ import * as dotenv from 'dotenv';
 dotenv.config({
     path: './test/.env.test',
 });
-
-global.console = {
-    ...console,
-    log: jest.fn(),
-    debug: jest.fn(),
-    info: jest.fn(),
-    warn: jest.fn(),
-    error: jest.fn(),
-};
