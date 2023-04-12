df7c4106bdeeb0e851ee2e66b42821d81a33f1d0
--get-all <271emtiaj@gmail.com>
2023-04-12 15:18:35 +0600

    disabled logs for npm script test:memory-usage

--- test/jest-init.ts
+++ test/jest-init.ts
@@ -1,5 +1,15 @@
 import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
 
 dotenv.config({
     path: './test/.env.test',
 });
+
+if (process.env.LOGGING_CONSOLE_ENABLED === 'false') {
+    process.env.TYPEORM_LOGGING = 'false';
+    jest.spyOn(Logger.prototype, 'log').mockImplementation();
+    jest.spyOn(Logger.prototype, 'error').mockImplementation();
+    jest.spyOn(Logger.prototype, 'debug').mockImplementation();
+    jest.spyOn(Logger.prototype, 'verbose').mockImplementation();
+    jest.spyOn(Logger.prototype, 'warn').mockImplementation();
+}
