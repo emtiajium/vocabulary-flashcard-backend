15d015c7197cb66da98c1b011a10cd823ab87fdd
--get-all <271emtiaj@gmail.com>
2024-02-21 19:46:04 +0600

    fixed migration issue in AWS EB

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm install && npm run start:prod
+web: npm install && npm run start:prod:aeb
--- src/common/persistence/TypeormConfig.ts
+++ src/common/persistence/TypeormConfig.ts
@@ -1,18 +1,34 @@
+/* eslint-disable node/no-process-env */
+
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import DatabaseConfig from '@/common/persistence/DatabaseConfig';
 import { ConfigService } from '@nestjs/config';
+import { execSync } from 'node:child_process';
 import * as process from 'node:process';
 
-config();
+const configService = new ConfigService();
 
-// eslint-disable-next-line node/no-process-env,no-console
-console.log('Testing in AEB', process.env);
+if (process.env.FIRECRACKER_PLATFORM === 'aeb') {
+    const environmentVariablesAsString = execSync(`/opt/elasticbeanstalk/bin/get-config environment`, {
+        encoding: 'utf8',
+    }).trim();
+    const environmentVariables = JSON.parse(environmentVariablesAsString);
+    configService.set('TYPEORM_CONNECTION', environmentVariables.TYPEORM_CONNECTION);
+    configService.set('TYPEORM_HOST', environmentVariables.TYPEORM_HOST);
+    configService.set('TYPEORM_PORT', environmentVariables.TYPEORM_PORT);
+    configService.set('TYPEORM_USERNAME', environmentVariables.TYPEORM_USERNAME);
+    configService.set('TYPEORM_PASSWORD', environmentVariables.TYPEORM_PASSWORD);
+    configService.set('TYPEORM_DATABASE', environmentVariables.TYPEORM_DATABASE);
+    configService.set('TYPEORM_LOGGING', environmentVariables.TYPEORM_LOGGING);
+    configService.set('TYPEORM_CONNECTION', environmentVariables.TYPEORM_CONNECTION);
+    configService.set('TYPEORM_MIGRATIONS', environmentVariables.TYPEORM_MIGRATIONS);
+} else {
+    config();
+}
 
 const persistence = new DatabaseConfig();
 const { host, port, username, password, database, type } = persistence;
-
-const configService = new ConfigService();
 
 export default new DataSource({
     type,
