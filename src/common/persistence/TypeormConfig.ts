80521aca88cb5572ecb395f495d4159ee403d2c4
Emtiaj Hasan <271emtiaj@gmail.com>
2026-02-08 01:56:00 +0000

    fixed env issue

--- src/common/persistence/TypeormConfig.ts
+++ src/common/persistence/TypeormConfig.ts
@@ -2,31 +2,10 @@ import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 import DatabaseConfig from '@/common/persistence/DatabaseConfig';
 import { ConfigService } from '@nestjs/config';
-import { execSync } from 'node:child_process';
-import * as process from 'node:process';
 
 const configService = new ConfigService();
 
-if (process.env.FIRECRACKER_PLATFORM === 'aeb') {
-    config();
-    // eslint-disable-next-line no-console
-    console.log('here+++', process.env);
-    const environmentVariablesAsString = execSync(`/opt/elasticbeanstalk/bin/get-config environment`, {
-        encoding: 'utf8',
-    }).trim();
-    const environmentVariables = JSON.parse(environmentVariablesAsString);
-    configService.set('TYPEORM_CONNECTION', environmentVariables.TYPEORM_CONNECTION);
-    configService.set('TYPEORM_HOST', environmentVariables.TYPEORM_HOST);
-    configService.set('TYPEORM_PORT', environmentVariables.TYPEORM_PORT);
-    configService.set('TYPEORM_USERNAME', environmentVariables.TYPEORM_USERNAME);
-    configService.set('TYPEORM_PASSWORD', environmentVariables.TYPEORM_PASSWORD);
-    configService.set('TYPEORM_DATABASE', environmentVariables.TYPEORM_DATABASE);
-    configService.set('TYPEORM_LOGGING', environmentVariables.TYPEORM_LOGGING);
-    configService.set('TYPEORM_CONNECTION', environmentVariables.TYPEORM_CONNECTION);
-    configService.set('TYPEORM_MIGRATIONS', environmentVariables.TYPEORM_MIGRATIONS);
-} else {
-    config();
-}
+config();
 
 const persistence = new DatabaseConfig();
 const { host, port, username, password, database, type } = persistence;
